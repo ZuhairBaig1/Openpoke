ac4e36287f1be52446a2832a95745e903164d4a7
Zuhair Baig <zuhair.md.baig@gmail.com>
2026-01-25 23:19:40 +0530

    Added logger.info for debugging

--- server/services/jira/client.py
+++ server/services/jira/client.py
@@ -158,7 +158,7 @@ def jira_initiate_connect(payload: JiraConnectPayload, settings: Settings) -> JS
         req = client.connected_accounts.initiate(
             user_id=user_id, 
             auth_config_id=auth_config_id, 
-            config={
+            properties={
                 "subdomain": subdomain,
                 "Your Subdomain": subdomain,
                 "authScheme": "OAUTH2"
